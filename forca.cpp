84e39f5a9e0187dfdee98d6e468bb008aa25ff28
vinibleik <vinicius_baggio@hotmail.com>
2020-01-03 15:50:56 -0300

    Menu Inicial

--- forca.cpp
+++ forca.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 void menu();
-void opcao();
+void limpatela();
 
 int main(int argc, char const *argv[]){
 
@@ -18,10 +18,43 @@ int main(int argc, char const *argv[]){
 	return 0;
 }
 
+//Menu inicial
 void menu(){
-	cout << "oi";
-}
 
-void opcao(){
-	cout << "teste"
+	int opcao;
+
+	do{
+		
+		cout << "-----Bem vindo ao jogo-----";
+		cout << "\n1) - Jogo sozinho";
+		cout << "\n2) - Jogo em dupla";
+		cout << "\n3) - Sobre";
+		cout << "\n4) - Sair";
+		cout << "\nDigite uma opcao: ";
+		cin >> opcao;
+
+		limpatela();
+
+		switch(opcao){
+			case 1:
+				cout << "opcao 1";
+				break;
+			case 2:
+				cout << "opcao 2";
+				break;
+			case 3:
+				cout << "opcao 3";
+				break;
+			case 4:
+				cout << "Obrigado por jogar";
+				break;
+			default :
+				opcao = 1;
+				cout << "Digite Apenas as opcoes disponiveis!!!!!\n";
+		}
+	} while (opcao>0 && opcao<4);
+
+}
+void limpatela(){
+	system("cls");
 }
