3f400c7837ab889b18266dc1ac898c9e3708dce7
vinibleik <vinicius_baggio@hotmail.com>
2020-01-02 22:49:05 -0300

    Primeiro commit

---
+++ forca.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+void menu();
+void opcao();
+
+int main(int argc, char const *argv[]){
+
+	menu();
+
+	return 0;
+}
+
+void menu(){
+	cout << "oi";
+}
